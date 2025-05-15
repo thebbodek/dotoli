4ce9e2e3f9eaf9895b05ef851a67125617fd8509
JinJu <jin_jooo@naver.com>
2025-05-15 14:47:57 +0900

    DOTOLI-22 BoxShadow Variants

---
+++ apps/internal-ui/src/variants/shadow/variant.ts
@@ -0,0 +1,8 @@
+export const SHADOW_VARIANTS = {
+  SHADOW_4: 'shadow-4',
+  SHADOW_8: 'shadow-8',
+  SHADOW_12: 'shadow-12',
+  SHADOW_20: 'shadow-20',
+  SHADOW_24: 'shadow-24',
+  SHADOW_30: 'shadow-30',
+} as const;
