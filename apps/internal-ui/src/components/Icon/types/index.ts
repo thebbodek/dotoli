a5b4c505d50d33dbe444d80ac4ed66fb6f48d7f7
baegofda <molly.daleo3o@gmail.com>
2025-05-16 17:22:09 +0900

    DOTOLI-85 Icon Component 타입 축소

--- apps/internal-ui/src/components/Icon/types/index.ts
+++ apps/internal-ui/src/components/Icon/types/index.ts
@@ -1,6 +1,8 @@
 import { IconStyle, PhosphorIcon } from '@phosphor-icons/core';
+import { HTMLAttributes } from 'react';
 
-export interface IconProps extends React.HTMLAttributes<HTMLSpanElement> {
+export interface IconProps
+  extends Pick<HTMLAttributes<HTMLSpanElement>, 'className' | 'title'> {
   iconKey: PhosphorIcon['name'];
   weight?: `${IconStyle}`;
 }
