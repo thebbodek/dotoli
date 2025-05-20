0b2b4ffdbbd1e942a60d203e0c60c44a2d6761dc
baegofda <molly.daleo3o@gmail.com>
2025-05-20 20:01:28 +0900

    DOTOLI-28 Radio Component

---
+++ apps/internal-ui/src/components/Radio/constants/index.ts
@@ -0,0 +1,40 @@
+import { RadioSize } from '@/components/Radio/types';
+import { TypographyVariants } from '@/variants';
+
+export const RADIO_SIZES = {
+  SM: 'sm',
+  MD: 'md',
+} as const;
+
+export const RADIO_STATE = {
+  DEFAULT: 'default',
+  SELECTED: 'selected',
+  DISABLED: 'disabled',
+} as const;
+
+export const RADIO_ICON_SIZE_STYLES_MAPPER: Record<RadioSize, string> = {
+  [RADIO_SIZES.SM]:
+    'h-[1.125rem] before:h-[0.6875rem] before:w-[0.6875rem] w-[1.125rem]',
+  [RADIO_SIZES.MD]: 'h-6 before:h-[0.875rem] before:w-[0.875rem] w-6',
+};
+
+export const RADIO_LABEL_SIZE_VARIANTS: Record<RadioSize, TypographyVariants> =
+  {
+    [RADIO_SIZES.SM]: 'body-14-r',
+    [RADIO_SIZES.MD]: 'body-16-r',
+  };
+
+export const RADIO_STYLES_MAPPER = {
+  [RADIO_STATE.DEFAULT]: {
+    icon: 'before:bg-white border-gray-02 bg-white',
+    label: 'text-black',
+  },
+  [RADIO_STATE.SELECTED]: {
+    icon: 'group-has-checked:before:bg-primary-06 group-has-checked:border-primary-06',
+    label: 'group-has-checked:text-black',
+  },
+  [RADIO_STATE.DISABLED]: {
+    icon: 'group-has-disabled:before:bg-gray-02 group-has-disabled:border-gray-02',
+    label: 'group-has-disabled:text-gray-05',
+  },
+} as const;
