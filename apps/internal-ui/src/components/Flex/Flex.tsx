6970d1eb0cdc6c04dfda1ac626b5f6848a15602b
baegofda <molly.daleo3o@gmail.com>
2025-05-22 06:02:42 +0900

    DOTOLI-86 Flex Component

---
+++ apps/internal-ui/src/components/Flex/Flex.tsx
@@ -0,0 +1,116 @@
+import clsx from 'clsx';
+import { PropsWithChildren } from 'react';
+
+import {
+  FLEX_ALIGN_STYLES,
+  FLEX_BASIS_STYLES,
+  FLEX_DIRECTION_STYLES,
+  FLEX_DIRECTIONS,
+  FLEX_ELEMENTS,
+  FLEX_GAP_STYLES,
+  FLEX_GAP_STYLES_BY_DIRECTION,
+  FLEX_GROW_AND_SHRINK_AND_BASIS_STYLES,
+  FLEX_GROW_STYLES,
+  FLEX_JUSTIFY_STYLES,
+  FLEX_ORDER_STYLES,
+  FLEX_SHRINK_STYLES,
+  FLEX_WRAP_STYLES,
+} from '@/components/Flex/constants';
+import {
+  FlexAlignOptions,
+  FlexAlignStyleValues,
+  FlexElementType,
+  FlexJustifyOptions,
+  FlexJustifyStyleValues,
+  FlexProps,
+} from '@/components/Flex/types';
+
+const Flex = <T extends FlexElementType = typeof FLEX_ELEMENTS.DIV>({
+  as: Element,
+  children,
+  className,
+  ...options
+}: PropsWithChildren<FlexProps<T>>) => {
+  const Component: FlexElementType = Element || FLEX_ELEMENTS.DIV;
+
+  const getOptions = ({
+    options,
+  }: {
+    options: Omit<FlexProps, 'children' | 'className'>;
+  }) => {
+    const {
+      direction = FLEX_DIRECTIONS.ROW,
+      align,
+      justify,
+      gap,
+      wrap,
+      basis,
+      shrink,
+      grow,
+      flex,
+      order,
+    } = options;
+
+    const classes = [
+      FLEX_DIRECTION_STYLES[direction],
+      wrap && FLEX_WRAP_STYLES[wrap],
+      basis && FLEX_BASIS_STYLES[basis],
+      shrink && FLEX_SHRINK_STYLES[shrink],
+      grow && FLEX_GROW_STYLES[grow],
+      flex && FLEX_GROW_AND_SHRINK_AND_BASIS_STYLES[flex],
+      order && FLEX_ORDER_STYLES[order],
+    ].filter(Boolean) as string[];
+
+    if (gap) {
+      if (typeof gap === 'number') {
+        classes.push(FLEX_GAP_STYLES[gap]);
+      }
+
+      if (gap instanceof Object) {
+        const { column, row } = gap;
+
+        if (column) {
+          classes.push(FLEX_GAP_STYLES_BY_DIRECTION.column[column]);
+        }
+
+        if (row) {
+          classes.push(FLEX_GAP_STYLES_BY_DIRECTION.row[row]);
+        }
+      }
+    }
+
+    if (align) {
+      Object.entries(align).forEach(([key, value]) => {
+        if (!key || !value) return;
+
+        classes.push(
+          FLEX_ALIGN_STYLES[key as FlexAlignOptions][
+            value as keyof FlexAlignStyleValues
+          ],
+        );
+      });
+    }
+
+    if (justify) {
+      Object.entries(justify).forEach(([key, value]) => {
+        if (!key || !value) return;
+
+        classes.push(
+          FLEX_JUSTIFY_STYLES[key as FlexJustifyOptions][
+            value as keyof FlexJustifyStyleValues
+          ],
+        );
+      });
+    }
+
+    return classes;
+  };
+
+  return (
+    <Component className={clsx(className, 'flex', ...getOptions({ options }))}>
+      {children}
+    </Component>
+  );
+};
+
+export default Flex;
---
+++ apps/storybook/src/stories/internal-ui/Flex.stories.tsx
@@ -0,0 +1,322 @@
+import { generateArgTypeSummary } from '@/utils/generateArgTypeSummary';
+import {
+  Flex,
+  FLEX_ALIGN_CONTENT_STYLES,
+  FLEX_ALIGN_ITEM_STYLES,
+  FLEX_ALIGN_SELF_STYLES,
+  FLEX_BASES,
+  FLEX_DIRECTIONS,
+  FLEX_ELEMENTS,
+  FLEX_GAPS,
+  FLEX_GROW_AND_SHRINK_AND_BASIS,
+  FLEX_GROWS,
+  FLEX_JUSTIFY_CONTENT_STYLES,
+  FLEX_JUSTIFY_CONTENTS,
+  FLEX_JUSTIFY_ITEM_STYLES,
+  FLEX_JUSTIFY_SELF_STYLES,
+  FLEX_ORDERS,
+  FLEX_SHRINKS,
+  FLEX_WRAPS,
+  FlexAlignContent,
+  FlexAlignItems,
+  FlexAlignSelf,
+  FlexGap,
+  FlexJustifyContent,
+  FlexJustifyItems,
+  FlexJustifySelf,
+  FlexProps,
+} from '@bbodek/internal-ui';
+import type { Meta } from '@storybook/react';
+
+interface FlexArgs extends Omit<FlexProps, 'align' | 'justify' | 'gap'> {
+  alignContent: FlexAlignContent;
+  alignItems: FlexAlignItems;
+  alignSelf: FlexAlignSelf;
+  justifyContent: FlexJustifyContent;
+  justifyItems: FlexJustifyItems;
+  justifySelf: FlexJustifySelf;
+  gap: FlexGap;
+  gapColumn: FlexGap;
+  gapRow: FlexGap;
+}
+
+const meta: Meta<FlexArgs> = {
+  title: 'core/internal-ui/Flex',
+  component: Flex,
+  argTypes: {
+    as: {
+      control: 'select',
+      options: Object.values(FLEX_ELEMENTS),
+      description: 'Flex element',
+      table: {
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_ELEMENTS),
+          }),
+        },
+        defaultValue: {
+          summary: FLEX_ELEMENTS.DIV,
+        },
+      },
+    },
+    direction: {
+      control: 'select',
+      options: Object.values(FLEX_DIRECTIONS),
+      description: 'Flex direction',
+      table: {
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_DIRECTIONS),
+          }),
+        },
+        defaultValue: {
+          summary: FLEX_DIRECTIONS.ROW,
+        },
+      },
+    },
+    alignContent: {
+      table: {
+        category: 'align',
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.keys(FLEX_ALIGN_CONTENT_STYLES),
+          }),
+        },
+      },
+      name: 'content',
+      control: 'select',
+      options: Object.keys(FLEX_ALIGN_CONTENT_STYLES),
+    },
+    alignItems: {
+      table: {
+        category: 'align',
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.keys(FLEX_ALIGN_ITEM_STYLES),
+          }),
+        },
+      },
+      name: 'items',
+      control: 'select',
+      options: Object.keys(FLEX_ALIGN_ITEM_STYLES),
+    },
+    alignSelf: {
+      table: {
+        category: 'align',
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.keys(FLEX_ALIGN_SELF_STYLES),
+          }),
+        },
+      },
+      name: 'self',
+      control: 'select',
+      options: Object.keys(FLEX_ALIGN_SELF_STYLES),
+    },
+    justifyContent: {
+      table: {
+        category: 'justify',
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.keys(FLEX_JUSTIFY_CONTENT_STYLES),
+          }),
+        },
+      },
+      name: 'content',
+      control: 'select',
+      options: Object.keys(FLEX_JUSTIFY_CONTENT_STYLES),
+    },
+    justifyItems: {
+      table: {
+        category: 'justify',
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.keys(FLEX_JUSTIFY_ITEM_STYLES),
+          }),
+        },
+      },
+      name: 'items',
+      control: 'select',
+      options: Object.keys(FLEX_JUSTIFY_ITEM_STYLES),
+    },
+    justifySelf: {
+      table: {
+        category: 'justify',
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.keys(FLEX_JUSTIFY_SELF_STYLES),
+          }),
+        },
+      },
+      name: 'self',
+      control: 'select',
+      options: Object.keys(FLEX_JUSTIFY_SELF_STYLES),
+    },
+    gap: {
+      table: {
+        category: 'gap',
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_GAPS),
+          }),
+        },
+      },
+      name: 'gap',
+      control: 'select',
+      options: Object.values(FLEX_GAPS),
+    },
+    gapColumn: {
+      table: {
+        category: 'gap',
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_GAPS),
+          }),
+        },
+      },
+      name: 'gap-column',
+      control: 'select',
+      options: Object.values(FLEX_GAPS),
+    },
+    gapRow: {
+      table: {
+        category: 'gap',
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_GAPS),
+          }),
+        },
+      },
+      name: 'gap-row',
+      control: 'select',
+      options: Object.values(FLEX_GAPS),
+    },
+    wrap: {
+      control: 'select',
+      options: Object.values(FLEX_WRAPS),
+      description: 'Flex wrap',
+      table: {
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_WRAPS),
+          }),
+        },
+      },
+    },
+    basis: {
+      control: 'select',
+      options: Object.values(FLEX_BASES),
+      description: 'Flex basis',
+      table: {
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_BASES),
+          }),
+        },
+      },
+    },
+    shrink: {
+      control: 'select',
+      options: Object.values(FLEX_SHRINKS),
+      description: 'Flex shrink',
+      table: {
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_SHRINKS),
+          }),
+        },
+      },
+    },
+    grow: {
+      control: 'select',
+      options: Object.values(FLEX_GROWS),
+      description: 'Flex grow',
+      table: {
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_GROWS),
+          }),
+        },
+      },
+    },
+    flex: {
+      control: 'select',
+      options: Object.values(FLEX_GROW_AND_SHRINK_AND_BASIS),
+      description: 'Flex flex',
+      table: {
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_GROW_AND_SHRINK_AND_BASIS),
+          }),
+        },
+      },
+    },
+    order: {
+      control: 'select',
+      options: Object.values(FLEX_ORDERS),
+      description: 'Flex order',
+      table: {
+        type: {
+          summary: generateArgTypeSummary({
+            options: Object.values(FLEX_ORDERS),
+          }),
+        },
+      },
+    },
+  },
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    children: 'Flex',
+  },
+};
+
+export const WithChildren = {
+  render: (args: FlexArgs) => {
+    const {
+      alignContent,
+      alignItems,
+      alignSelf,
+      justifyContent = FLEX_JUSTIFY_CONTENTS.CENTER,
+      justifyItems,
+      justifySelf,
+      gap,
+      gapColumn,
+      gapRow,
+      ...originalProps
+    } = args;
+
+    const align = {
+      content: alignContent,
+      items: alignItems,
+      self: alignSelf,
+    };
+
+    const justify = {
+      content: justifyContent,
+      items: justifyItems,
+      self: justifySelf,
+    };
+
+    const items = Array.from({ length: 10 }, (_, index) => (
+      <div key={index} className='shadow-12 rounded-4 px-2 py-1'>
+        Flex {index + 1}
+      </div>
+    ));
+
+    return (
+      <Flex
+        className='w-[1000px] px-4'
+        {...originalProps}
+        align={align}
+        justify={justify}
+        gap={gap || { column: gapColumn, row: gapRow }}
+      >
+        {items}
+      </Flex>
+    );
+  },
+};
