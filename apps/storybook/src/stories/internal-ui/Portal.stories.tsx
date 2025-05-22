d5fc59c48e092ea51903b69241c4bb5232574f7b
baegofda <molly.daleo3o@gmail.com>
2025-05-22 23:01:40 +0900

    DOTOLI-89 Portal Component

---
+++ apps/storybook/src/stories/internal-ui/Portal.stories.tsx
@@ -0,0 +1,50 @@
+import { Button, Portal, PORTAL_DEFAULT_TARGET } from '@bbodek/internal-ui';
+import { Meta } from '@storybook/react';
+import { useState } from 'react';
+
+const meta: Meta<typeof Portal> = {
+  title: 'core/internal-ui/Portal',
+  component: Portal,
+  argTypes: {
+    target: {
+      description: 'Portal target',
+      control: 'text',
+      table: {
+        defaultValue: {
+          summary: PORTAL_DEFAULT_TARGET,
+        },
+        type: {
+          summary: 'string',
+        },
+      },
+    },
+  },
+};
+
+export default meta;
+
+export const Default = {
+  render: () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onToggle = () => {
+      setIsOpen((v) => !v);
+    };
+
+    return (
+      <div className='flex flex-col gap-4'>
+        <Button label='Open Portal' onClick={onToggle} />
+        <div className='bg-red-03'>
+          <div id='portal' />
+        </div>
+        {isOpen && (
+          <div className='bg-green-03'>
+            <div>
+              <Portal>Portal</Portal>Original Portal Position
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  },
+};
