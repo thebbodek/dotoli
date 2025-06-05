a6b099901e7de89b5f624db93b4a03772545c8a0
baegofda <molly.daleo3o@gmail.com>
2025-06-05 17:48:52 +0900

    DOTOLI-93 Select Component

---
+++ apps/internal-ui/src/components/Select/Single/Select/SelectItem.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from 'react';
+
+import { SelectItemProps } from '@/components/Select/Single/Select/types';
+import { SingleSelectBaseItem } from '@/components/Select/Single/shared';
+import { SelectValue } from '@/components/Select/Single/shared/types';
+
+const SelectItem = <T extends SelectValue>({
+  children,
+  value,
+}: PropsWithChildren<SelectItemProps<T>>) => {
+  return <SingleSelectBaseItem value={value}>{children}</SingleSelectBaseItem>;
+};
+
+export default SelectItem;
---
+++ apps/internal-ui/src/components/Select/Single/shared/SingleSelectBase.tsx
@@ -0,0 +1,64 @@
+import { PropsWithChildren, useId } from 'react';
+
+import { SelectBase } from '@/components/Select/shared';
+import { SingleSelectListProvider } from '@/components/Select/Single/shared/context';
+import SingleSelectBaseList from '@/components/Select/Single/shared/SingleSelectBaseList';
+import SingleSelectTrigger from '@/components/Select/Single/shared/SingleSelectBaseTrigger';
+import {
+  SelectValue,
+  SingleSelectBaseProps,
+} from '@/components/Select/Single/shared/types';
+
+const SingleSelectBase = <T extends SelectValue>({
+  className,
+  label,
+  popoverOption,
+  disabled,
+  badge,
+  feedback,
+  required,
+  isError,
+  onSelect,
+  value,
+  displayValue,
+  placeholder,
+  type,
+  children,
+  popoverWrapperClassName,
+}: PropsWithChildren<SingleSelectBaseProps<T>>) => {
+  const selectListId = useId();
+  const labelId = useId();
+
+  return (
+    <SelectBase
+      type={type}
+      className={className}
+      label={<SelectBase.Label badge={badge}>{label}</SelectBase.Label>}
+      popoverOption={popoverOption}
+      trigger={<SingleSelectTrigger displayValue={displayValue} />}
+      disabled={disabled}
+      feedback={feedback}
+      required={required}
+      isError={isError}
+      placeholder={placeholder}
+      controls={selectListId}
+      labelId={labelId}
+    >
+      {({ close }) => (
+        <SingleSelectListProvider
+          id={selectListId}
+          labelId={labelId}
+          value={value}
+          onSelect={onSelect}
+          close={close}
+        >
+          <SelectBase.PopoverWrapper className={popoverWrapperClassName}>
+            <SingleSelectBaseList>{children}</SingleSelectBaseList>
+          </SelectBase.PopoverWrapper>
+        </SingleSelectListProvider>
+      )}
+    </SelectBase>
+  );
+};
+
+export default SingleSelectBase;
