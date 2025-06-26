c8b191cb1fb0ae5362a014672357389f68e3410e
JinJu <jin_jooo@naver.com>
2025-06-26 16:00:40 +0900

    DOTOLI-36 Sort Component

---
+++ apps/internal-ui/src/components/Sort/types/index.ts
@@ -0,0 +1,33 @@
+import { HTMLAttributes } from 'react';
+
+import {
+  SelectValue,
+  SingleSelectBaseItemProps,
+  SingleSelectBaseProps,
+  SingleSelectOnSelectParams,
+} from '@/components/Select';
+
+export type SortValue = SelectValue;
+
+export interface SortProps
+  extends Pick<
+    SingleSelectBaseProps<SortValue>,
+    | 'value'
+    | 'disabled'
+    | 'popoverOption'
+    | 'onSelect'
+    | 'displayValue'
+    | 'className'
+    | 'label'
+    | 'badge'
+  > {}
+
+export interface SortOnSelectParams<T extends SortValue>
+  extends SingleSelectOnSelectParams<T> {}
+
+export interface SortTriggerProps
+  extends Pick<HTMLAttributes<HTMLDivElement>, 'className'>,
+    Pick<SortProps, 'displayValue'> {}
+
+export interface SortItemProps<T extends SortValue>
+  extends Pick<SingleSelectBaseItemProps<T>, 'value' | 'label'> {}
--- apps/internal-ui/src/components/index.ts
+++ apps/internal-ui/src/components/index.ts
@@ -17,6 +17,7 @@ export * from './Popover';
 export * from './Portal';
 export * from './Radio';
 export * from './Select';
+export * from './Sort';
 export * from './Tab';
 export * from './Table';
 export * from './Toggle';
