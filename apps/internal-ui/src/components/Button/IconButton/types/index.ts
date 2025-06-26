d497ece91a2c522d038868c730dc6381b9ca110d
JinJu <jin_jooo@naver.com>
2025-06-26 10:37:34 +0900

    DOTOLI-105 Add IconButton Tooltip, bgWhite Theme

--- apps/internal-ui/src/components/Button/IconButton/types/index.ts
+++ apps/internal-ui/src/components/Button/IconButton/types/index.ts
@@ -4,6 +4,7 @@ import { ButtonProps } from '@/components/Button/Button';
 import { ICON_BUTTON_THEMES } from '@/components/Button/IconButton/constants';
 import { IconProps } from '@/components/Icon';
 import { ComponentPropsRef } from '@/components/shared';
+import { TooltipProps } from '@/components/Tooltip';
 
 export type IconButtonTheme =
   (typeof ICON_BUTTON_THEMES)[keyof typeof ICON_BUTTON_THEMES];
@@ -11,11 +12,13 @@ export type IconButtonTheme =
 export interface IconButtonProps
   extends Pick<
       ButtonHTMLAttributes<HTMLButtonElement>,
-      'className' | 'onClick' | 'disabled' | 'type'
+      'onClick' | 'disabled' | 'type'
     >,
+    Pick<TooltipProps, 'rootClassName'>,
     Pick<IconProps, 'iconKey'>,
     Pick<ButtonProps, 'isPending'>,
     ComponentPropsRef<HTMLButtonElement> {
   theme?: IconButtonTheme;
   arialLabel: Required<ButtonHTMLAttributes<HTMLButtonElement>['aria-label']>;
+  tooltipContent?: string;
 }
