4a48d82c1ab3474deb50a4c04e53b47d4e159bce
JinJu <jin_jooo@naver.com>
2025-06-26 10:37:34 +0900

    DOTOLI-105 Add IconButton Tooltip, bgWhite Theme

--- apps/internal-ui/src/components/Button/IconButton/IconButton.tsx
+++ apps/internal-ui/src/components/Button/IconButton/IconButton.tsx
@@ -7,15 +7,17 @@ import {
 } from '@/components/Button/IconButton/constants';
 import { BUTTON_PENDING_ICON_KEY } from '@/components/Button/shared';
 import { Icon } from '@/components/Icon';
+import { Tooltip } from '@/components/Tooltip';
 import { IconButtonProps } from './types';
 
 const IconButton = ({
   ref,
   iconKey,
   type = 'button',
-  theme = ICON_BUTTON_THEMES.GRAY,
+  theme = ICON_BUTTON_THEMES.HOVER_GRAY,
   arialLabel,
-  className,
+  tooltipContent,
+  rootClassName,
   disabled = false,
   isPending = false,
   onClick,
@@ -31,23 +33,29 @@ const IconButton = ({
   };
 
   return (
-    <button
-      ref={ref}
-      type={type}
-      className={clsx(
-        'rounded-6 text-gray-06 text-body-16-b active:text-primary-05 disabled:text-gray-04 flex h-8 w-8 cursor-pointer items-center justify-center transition-colors disabled:cursor-not-allowed',
-        ICON_BUTTON_COLORS_STYLES[theme],
-        className,
-      )}
-      aria-label={arialLabel}
-      disabled={isDisabled}
-      onClick={handleClick}
+    <Tooltip
+      content={tooltipContent ?? arialLabel}
+      rootClassName={rootClassName}
+      placement='bottom'
     >
-      <Icon
-        iconKey={isPending ? BUTTON_PENDING_ICON_KEY : iconKey}
-        className={clsx(isPending && 'animate-spin')}
-      />
-    </button>
+      <button
+        ref={ref}
+        type={type}
+        className={clsx(
+          'rounded-6 text-gray-06 text-body-16-b disabled:text-gray-04 flex-h-stack-center h-8 w-8 cursor-pointer transition-colors disabled:cursor-not-allowed',
+          ICON_BUTTON_COLORS_STYLES[theme],
+        )}
+        aria-label={arialLabel}
+        disabled={isDisabled}
+        onClick={handleClick}
+      >
+        <Icon
+          iconKey={isPending ? BUTTON_PENDING_ICON_KEY : iconKey}
+          className={clsx(isPending && 'animate-spin')}
+          aria-hidden
+        />
+      </button>
+    </Tooltip>
   );
 };
 
--- apps/internal-ui/src/components/Button/IconButton/constants/index.ts
+++ apps/internal-ui/src/components/Button/IconButton/constants/index.ts
@@ -1,12 +1,15 @@
 import { IconButtonTheme } from '@/components/Button/IconButton/types';
 
 export const ICON_BUTTON_THEMES = {
-  GRAY: 'gray',
-  WHITE: 'white',
+  HOVER_GRAY: 'hover-gray',
+  HOVER_WHITE: 'hover-white',
+  BG_WHITE: 'bg-white',
 } as const;
 
 export const ICON_BUTTON_COLORS_STYLES: Record<IconButtonTheme, string> = {
-  [ICON_BUTTON_THEMES.GRAY]:
-    'hover:bg-primary-01 active:bg-primary-01 disabled:bg-transparent',
-  [ICON_BUTTON_THEMES.WHITE]: '',
+  [ICON_BUTTON_THEMES.HOVER_GRAY]:
+    'hover:bg-primary-01 active:bg-primary-01 disabled:bg-transparent active:text-primary-05',
+  [ICON_BUTTON_THEMES.HOVER_WHITE]: 'active:text-primary-05',
+  [ICON_BUTTON_THEMES.BG_WHITE]:
+    'bg-white hover:bg-gray-01 disabled:bg-gray-01',
 };
