1db03bebcbd70075ca9ce13aeff1f869a3d26a53
baegofda <molly.daleo3o@gmail.com>
2025-07-15 02:54:06 +0900

    DOTOLI-30 DatePicker Component

---
+++ apps/internal-ui/src/components/DatePicker/DatePickerTrigger.tsx
@@ -0,0 +1,68 @@
+import dayjs from 'dayjs';
+
+import { CALENDAR_VARIANTS, useCalendarContext } from '@/components/Calendar';
+import { isValidDateOfVariant } from '@/components/DatePicker/utils';
+import { Icon } from '@/components/Icon';
+import {
+  SelectBaseTriggerWrapper,
+  useSelectTriggerContext,
+} from '@/components/Select/shared';
+import SelectBaseDisplayValue from '@/components/Select/shared/SelectBaseDisplayValue';
+import SelectBaseResetButton from '@/components/Select/shared/SelectBaseResetButton';
+
+const DatePickTrigger = () => {
+  const { placeholder = '', disabled } = useSelectTriggerContext();
+  const { value, variant, setInternalValue, onChange } = useCalendarContext();
+  const hasValue = !!value && isValidDateOfVariant({ value, variant });
+  const showResetButton = hasValue && !disabled;
+
+  const onReset = () => {
+    setInternalValue(null);
+    onChange(null);
+  };
+
+  const displayValue = () => {
+    if (!value) return placeholder;
+
+    const { startDate, endDate } = value;
+    const formattedStartDate = dayjs(startDate).format('YYYY-MM-DD');
+    const formattedEndDate = dayjs(endDate).format('YYYY-MM-DD');
+
+    if (
+      variant === CALENDAR_VARIANTS.SINGLE &&
+      isValidDateOfVariant({ value, variant: CALENDAR_VARIANTS.SINGLE })
+    ) {
+      return formattedStartDate;
+    }
+
+    if (
+      variant === CALENDAR_VARIANTS.RANGE &&
+      isValidDateOfVariant({ value, variant: CALENDAR_VARIANTS.RANGE })
+    ) {
+      return `${formattedStartDate} ~ ${formattedEndDate}`;
+    }
+
+    if (
+      variant === CALENDAR_VARIANTS.UNBOUNDED &&
+      isValidDateOfVariant({ value, variant: CALENDAR_VARIANTS.UNBOUNDED })
+    ) {
+      return `${formattedStartDate} 부터`;
+    }
+
+    return placeholder;
+  };
+
+  return (
+    <SelectBaseTriggerWrapper
+      subFixIcon={<Icon iconKey='calendar-dots' className='text-[1.125rem]' />}
+    >
+      <SelectBaseDisplayValue
+        displayValue={displayValue()}
+        hasValue={hasValue}
+      />
+      {showResetButton && <SelectBaseResetButton onClick={onReset} />}
+    </SelectBaseTriggerWrapper>
+  );
+};
+
+export default DatePickTrigger;
