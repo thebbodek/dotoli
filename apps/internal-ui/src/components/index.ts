35945be07d57dca9f80b1fe66822ef54ced84c35
JinJu <jin_jooo@naver.com>
2025-07-15 16:16:03 +0900

    DOTOLO-88 Toast Component

--- apps/internal-ui/src/components/index.ts
+++ apps/internal-ui/src/components/index.ts
@@ -20,6 +20,7 @@ export * from './Radio';
 export * from './Select';
 export * from './Tab';
 export * from './Table';
+export * from './Toast';
 export * from './Toggle';
 export * from './Tooltip';
 export * from './Typography';
