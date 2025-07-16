2a2d741fea554751df411ce1fdfc2e69aa3b6da3
JinJu <jin_jooo@naver.com>
2025-07-16 16:42:38 +0900

    DOTOLI-52 removeExt

---
+++ apps/storybook/src/stories/utils/removeExt.stories.tsx
@@ -0,0 +1,39 @@
+import { Button, Flex, Typography } from '@bbodek/internal-ui';
+import { ExtFnProps, removeExt } from '@bbodek/utils';
+import type { Meta, StoryObj } from '@storybook/react';
+import { useEffect, useState } from 'react';
+
+import { default as extractExtMeta } from './extractExt.stories';
+
+const meta: Meta<ExtFnProps> = {
+  title: 'core/utils/removeExt',
+  argTypes: extractExtMeta.argTypes,
+};
+
+export default meta;
+
+type Story = StoryObj<ExtFnProps>;
+
+export const Default: Story = {
+  args: {
+    str: 'test.png',
+    accept: ['png', 'jpeg'],
+  },
+  render: (args) => {
+    const [str, setStr] = useState(args.str);
+
+    useEffect(() => {
+      setStr(args.str);
+    }, [args.str]);
+
+    return (
+      <Flex direction='column' align={{ items: 'center' }} gap={4}>
+        <Typography>{str}</Typography>
+        <Button
+          label='확장자 제거하기'
+          onClick={() => setStr(removeExt({ str }))}
+        />
+      </Flex>
+    );
+  },
+};
--- apps/utils/src/index.ts
+++ apps/utils/src/index.ts
@@ -4,6 +4,7 @@ export * from '@/extractExt';
 export * from '@/highlightText';
 export * from '@/isServer';
 export * from '@/mergeRefs';
+export * from '@/removeExt';
 export * from '@/toast';
 
 export { ACCEPT_FILE_EXT, ACCEPT_FILES } from '@/shared';
