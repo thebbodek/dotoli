6fe4038ac3df416361b8aa95317b7be23a2f566e
JinJu <jin_jooo@naver.com>
2025-07-22 16:16:36 +0900

    DOTOLI-46 clipboard

---
+++ apps/storybook/src/stories/utils/clipboard.stories.tsx
@@ -0,0 +1,42 @@
+import { Button, Flex, InputField, Typography } from '@bbodek/internal-ui';
+import { clipboard } from '@bbodek/utils';
+import type { Meta } from '@storybook/react';
+import { useEffect, useState } from 'react';
+
+const meta: Meta = {
+  title: 'core/utils/clipboard',
+  argTypes: {
+    text: {
+      control: 'text',
+      type: 'string',
+    },
+  },
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    text: '이 텍스트가 복사됩니다',
+  },
+  render: ({ text }: keyof typeof meta.argTypes) => {
+    const [copyText, setCopyText] = useState(text);
+
+    useEffect(() => {
+      setCopyText(text);
+    }, [text]);
+
+    return (
+      <Flex direction='column' align={{ items: 'center' }} gap={4}>
+        <div className='in-flex-h-stack-center gap-4'>
+          <Typography>{copyText}</Typography>
+          <Button size='sm' label='복사하기' onClick={() => clipboard(text)} />
+        </div>
+        <InputField
+          placeholder='복사 후 여기에 붙여넣어 보세요!'
+          rootClassName='w-full'
+        />
+      </Flex>
+    );
+  },
+};
