e326e2f909847bb47b8e89297f40c9060155365a
JinJu <jin_jooo@naver.com>
2025-10-20 19:39:24 +0900

    DOTOLI-138 rollup watch mode build 환경 개선

--- apps/hooks/rollup.config.mjs
+++ apps/hooks/rollup.config.mjs
@@ -1,3 +1,7 @@
 import { createRollupConfig } from '@dotoli/rollup-config';
+import { fileURLToPath } from 'url';
 
-export default createRollupConfig({ external: ['next'] });
+export default createRollupConfig({
+  external: ['next'],
+  srcPath: fileURLToPath(new URL('./src', import.meta.url)),
+});
