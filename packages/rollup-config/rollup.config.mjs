919595e6101d3911c39ce01ceaa98d984ab3bdc0
JinJu <jin_jooo@naver.com>
2026-01-30 15:40:49 +0900

    DOTOLI-153 toast 외부 사용시 미노출 이슈 수정

--- packages/rollup-config/rollup.config.mjs
+++ packages/rollup-config/rollup.config.mjs
@@ -8,6 +8,16 @@ import watchConfig from './watch.mjs';
 
 const isWatch = process.argv.includes('--watch');
 
+/**
+ * @typedef {Object} CreateRollupConfigOptions
+ * @property {Array} [plugins] - 추가로 사용할 rollup 플러그인들의 배열
+ * @property {Array<string>} [external] - 외부 모듈로 간주할 패키지들의 배열
+ * @property {string} [srcPath] - src 디렉터리 절대 경로 (e.g. fileURLToPath(new URL('./src', import.meta.url)))
+ */
+
+/**
+ * @param {CreateRollupConfigOptions} options
+ */
 export const createRollupConfig = (options) => {
   const {
     plugins: externalPlugins = [],
