52d6f2e516664ef78fbcb64a76f9bd1caa2a4037
JinJu <jin_jooo@naver.com>
2026-03-11 15:04:51 +0900

    DOTOLI-166 Toaster id options 추가

--- apps/utils/src/toast/types/index.ts
+++ apps/utils/src/toast/types/index.ts
@@ -4,7 +4,7 @@ import { ToastOptions } from 'react-hot-toast';
 export interface CreateToastOption
   extends Omit<ToastProps, 'visible' | 'onClose'>,
     Partial<Pick<ToastProps, 'onClose'>>,
-    Pick<ToastOptions, 'duration'> {}
+    Pick<ToastOptions, 'duration' | 'id' | 'toasterId'> {}
 
 export interface ToastOption
   extends Omit<CreateToastOption, 'type' | 'content'> {}
